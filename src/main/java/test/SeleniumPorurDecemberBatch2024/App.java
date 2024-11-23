36f25906038f4da0d2a8189dbcf20dc000500785
sathish1980 <kumar.sathish189@gmail.com>
2024-11-23 14:36:01 +0530

    message

---
+++ src/main/java/test/SeleniumPorurDecemberBatch2024/App.java
@@ -0,0 +1,10 @@
+package test.SeleniumPorurDecemberBatch2024;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
