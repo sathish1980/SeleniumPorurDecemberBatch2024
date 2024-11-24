771ad611e7e40ebd95c165da2e5f70c5349c150c
sathish1980 <kumar.sathish189@gmail.com>
2024-11-24 15:46:43 +0530

    xpath

--- src/test/java/BasicsOfSelenium/FirstClass.java
+++ src/test/java/BasicsOfSelenium/FirstClass.java
@@ -14,12 +14,63 @@ public class FirstClass {
 		//browser.manage().window().minimize();
 
 		browser.get("https://www.facebook.com/");
-		browser.navigate().to("https://www.gmail.com/");
+		/*browser.navigate().to("https://www.gmail.com/");
 		browser.navigate().back();
 		browser.navigate().forward();
-		browser.navigate().refresh();
+		browser.navigate().refresh();*/
 		//browser.close();
 		//browser.quit();
+		
+		/*
+		 * locators
+		 */
+		WebElement username = browser.findElement(By.id("email"));
+		username.sendKeys("sathish");
+		username.clear();
+		
+		browser.findElement(By.name("email")).sendKeys("Besant");
+		
+		//browser.findElement(By.className("inputtext _55r1 _6luy")).sendKeys("Besant");
+	
+		//browser.findElement(By.name("login")).click();
+		
+		//browser.findElement(By.linkText("Forgotten password?")).click();
+		
+		//browser.findElement(By.partialLinkText("n pass")).click();
+		
+		browser.findElement(By.cssSelector("input#pass")).sendKeys("test");
+		
+		browser.findElement(By.cssSelector("input.inputtext _55r1 _6luy _9npi")).sendKeys("test");
+		
+		
+		browser.findElement(By.cssSelector("input[data-testid='royal_email']")).sendKeys("test");
+		
+		browser.findElement(By.cssSelector("input.inputtext _55r1 _6luy _9npi[data-testid='royal_email']")).sendKeys("test");
+		
+	
+		/*
+		 * startswith --tagname[attribute^='staring value']  or [attribute^='staring value']
+		 * endswith ----tagname[attribute$='end value']
+		 * contains ----tagname[attribute*='any where value']
+		 */
+	
+		/*
+		 * Xpath
+		 * 2 types
+		 * Absolut xpath =/
+		 * relative xpath = //
+		 * 5 type
+		 * 1.Basic xpath
+		 * 2.and or
+		 * 3.starts-with
+		 * 4.contains
+		 * 5.text
+		 * 
+		 */
+		
+		browser.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[2]/div/div/form/div/div/input")).sendKeys("test");
+		
+		browser.findElement(By.xpath("//input[@type='text']")).sendKeys("test");
 		
 	}
 
